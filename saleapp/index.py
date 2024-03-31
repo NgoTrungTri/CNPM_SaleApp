581e4d07e81df1bc63cbab3c9b7f5ffbc6219d48
NgoTrungTri <ngotrungtri1234@gmail.com>
2024-04-01 01:17:57 +0700

    Buổi 1

---
+++ saleapp/index.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+import dao
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    q = request.args.get("q")
+    cate_id = request.args.get("category_id")
+    products = dao.load_Products(q, cate_id)
+    return render_template('index.html', products=products)
+
+
+@app.route("/products/<int:id>")
+def details(id):
+    product = dao.get_product_by_id(product_id=id)
+    return render_template("product-details.html", product=product)
+
+
+@app.route("/login", methods=['get', 'post'])
+def login():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username.__eq__('admin') and password.__eq__('123'):
+            return redirect('/')
+
+    return render_template("login.html")
+
+
+@app.context_processor
+def common_atributes():
+    return {
+        'categories': dao.load_Categories()
+    }
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
